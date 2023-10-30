71ac8df18cae4ed56a0b8a65701972e986d45d10
durga0702 <durga@sparkouttech.com>
2023-10-30 09:59:53 +0530

    library mydemo

---
+++ projects/my-demo/src/lib/my-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-demo',
+  template: `
+    <p>
+      my-demo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyDemoComponent {
+
+}
---
+++ projects/my-demo/src/lib/my-demo.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MyDemoComponent } from './my-demo.component';
+
+
+
+@NgModule({
+  declarations: [
+    MyDemoComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    MyDemoComponent
+  ]
+})
+export class MyDemoModule { }
